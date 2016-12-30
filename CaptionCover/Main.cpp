e055dbec4bb2207dc7815e8e8eaa898bb9396e85
elnino <delongdal@outlook.com>
2016-12-30 23:19:22 +0800

    first commit

---
+++ CaptionCover/Main.cpp
@@ -0,0 +1,33 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace cv;
+using namespace std;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	string filename = "black.jpg";
+	string filename_0 = "black_0.jpg";		// Ö»ÓÐ±ß¿ò
+
+	Mat img = imread(filename_0);
+	namedWindow("ÕÚµ²´°¿Ú");//, WINDOW_NORMAL);
+
+	HWND hWnd = (HWND)cvGetWindowHandle("ÕÚµ²´°¿Ú");
+	HWND hRawWnd = ::GetParent(hWnd);
+	if (hRawWnd != NULL) {
+		BOOL bRet = ::SetWindowPos(hRawWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
+		assert(bRet);
+	}
+
+	imshow("ÕÚµ²´°¿Ú", img);
+	
+	while (true)
+	{
+		waitKey();
+	}
+
+}
